d3a68658140c8dd4252dea73da3d554d05f732c2
Junior Santos <gilsonsantosjunior02@gmail.com>
2022-02-04 18:03:43 -0300

    Exercício dados form

---
+++ Exercícios/Ex002/src/components/RenderizaçãoLista.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+function RenderizaçãoLista({itens}) {
+    return (
+        <>
+            <h3>Lista de livros</h3>
+            
+        </>
+    )
+}
+
+export default RenderizaçãoLista; 
